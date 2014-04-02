57f9532170be98173d23e94adfc279a08aab04d1
Craig Walls <cwalls@gopivotal.com>
2014-04-02 14:43:28 -0500

    Depend on a patched version of json-patch library instead of hack-patched in-project.

--- src/main/java/com/github/fge/jsonpatch/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This package exists in this project to hack-override classes from the JSON Patch project at https://github.com/fge/json-patch.
- * The overrides enable the patch operations to call into PatchListener implementations when a patch operation is performed.
- * This enables thing such as the "remove" operation to be performed not only on the JSON, but for the listener to properly delete the targeted 
- * entity in the database.
- * Without this, the patch would only be applied to the JSON representation of the document and it would be difficult (at best) to know when
- * to remove the deleted item from a collection.
- */
-package com.github.fge.jsonpatch;
