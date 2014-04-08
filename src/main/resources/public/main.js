2c3edb7ee3888a47fecdd2e9cb40260b31ee696d
Brian Cavalier <brian@hovercraftstudios.com>
2014-04-08 14:41:50 -0400

    Register json-patch mime-type during init
    Since rest.js's mime registry is a singleton, this seems fine for now.
    Ideally, we'd be able to create a rest client instance with its own
    registry and use that when constructing a JsonPatch instance.

--- src/main/resources/public/main.js
+++ src/main/resources/public/main.js
@@ -1,9 +1,14 @@
+var registry = require('rest/mime/registry');
+var json = require('rest/mime/type/application/json');
 var fluent = require('wire/config/fluent');
 var JsonPatch = require('cola/data/JsonPatch');
 var TodosController = require('./TodosController');
 
 module.exports = fluent(function(context) {
 	return context
+		.add('@init', function() {
+			return registry.register('application/json-patch+json', json)
+		})
 		.add('todos@controller', TodosController)
 		.add('todos@model', function() {
 			return new JsonPatch('/todos');
