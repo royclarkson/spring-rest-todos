c312533e1725786894aa4069a7257d29bee2dbd1
Brian Cavalier <brian@hovercraftstudios.com>
2014-04-09 14:01:57 -0400

    Add complete all, remove todo, remove all completed. Add bower resolution for when 3

--- src/main/resources/public/TodosController.js
+++ src/main/resources/public/TodosController.js
@@ -5,3 +5,25 @@ function TodosController() {}
 TodosController.prototype.add = function(todos, todo) {
 	todos.push(todo);
 };
+
+TodosController.prototype.remove = function(todos, todo) {
+	todos.some(function(t, i, todos) {
+		if(todo.id === t.id) {
+			todos.splice(i, 1);
+			return true;
+		}
+	});
+};
+
+TodosController.prototype.removeCompleted = function(todos) {
+	return todos.filter(function(todo) {
+		return !todo.complete;
+	});
+};
+
+TodosController.prototype.completeAll = function(todos) {
+	todos.forEach(function(todo) {
+		todo.complete = true;
+	});
+};
+
